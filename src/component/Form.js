6f3e93e8d94f93c48df1952d3eed9aaf4761564e
Michael Vega <micha3lvega@gmail.com>
2020-04-30 13:16:07 -0500

    create form

---
+++ src/component/Form.js
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from 'react';
+import { Container } from 'reactstrap';
+
+class Form extends Component {
+
+    render() {
+        return (
+            <Fragment>
+                <Container fluid>
+                    <h1>Hola mundo</h1>
+                </Container>
+            </Fragment>
+        )
+    }
+
+}
+
+export default Form
